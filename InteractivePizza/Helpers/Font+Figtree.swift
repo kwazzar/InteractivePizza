f8d2227275e1db162e2c41506537d8a29f742467
Nazar Vesnych <fugitiwe@gmail.com>
2026-09-15 00:47:40 +0300

    Move assets to Resources, add Figtree fonts, Font extension, and Info.plist

---
+++ InteractivePizza/Helpers/Font+Figtree.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+
+extension Font {
+    enum FigtreeWeight {
+        case light, regular, medium, bold, black
+
+        var fontWeight: Font.Weight {
+            switch self {
+            case .light:    return .light
+            case .regular:  return .regular
+            case .medium:   return .medium
+            case .bold:     return .bold
+            case .black:    return .black
+            }
+        }
+    }
+
+    static func figtree(_ weight: FigtreeWeight = .regular, size: CGFloat) -> Font {
+        .custom("Figtree", size: size).weight(weight.fontWeight)
+    }
+}
