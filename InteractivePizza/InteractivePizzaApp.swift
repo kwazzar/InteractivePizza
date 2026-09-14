ecc16217406908d4d6aec4000a588d22bf0ec1df
Nazar Vesnych <fugitiwe@gmail.com>
2026-09-14 15:04:21 +0300

    Initial Commit

---
+++ InteractivePizza/InteractivePizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractivePizzaApp.swift
+//  InteractivePizza
+//
+//  Created by kwazzar on 14.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct InteractivePizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
